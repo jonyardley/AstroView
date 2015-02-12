ae7c42a7e71c4fa4aae0b14e19971487c302a211
Jon Yardley <jon.yardley@digitaslbi.com>
2015-02-12 16:35:16 +0000

    Introduced a new main.js file to control configuratino. Useful for moving to production

---
+++ dist/main.js
@@ -0,0 +1,26 @@
+System.register(["aurelia-framework", "aurelia-logging-console"], function (_export) {
+  "use strict";
+
+  var LogManager, ConsoleAppender;
+  _export("configure", configure);
+
+  function configure(aurelia) {
+    aurelia.use.defaultBindingLanguage().defaultResources().router().eventAggregator();
+
+    aurelia.start().then(function (a) {
+      return a.setRoot("app", document.body);
+    });
+  }
+  return {
+    setters: [function (_aureliaFramework) {
+      LogManager = _aureliaFramework.LogManager;
+    }, function (_aureliaLoggingConsole) {
+      ConsoleAppender = _aureliaLoggingConsole.ConsoleAppender;
+    }],
+    execute: function () {
+      LogManager.addAppender(new ConsoleAppender());
+      LogManager.setLevel(LogManager.levels.info);
+    }
+  };
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O01BQVEsVUFBVSxFQUNWLGVBQWU7dUJBS1AsU0FBUzs7QUFBbEIsV0FBUyxTQUFTLENBQUMsT0FBTyxFQUFFO0FBQ2xDLFdBQU8sQ0FBQyxHQUFHLENBQ1Qsc0JBQXNCLEVBQUUsQ0FDeEIsZ0JBQWdCLEVBQUUsQ0FDbEIsTUFBTSxFQUFFLENBQ1IsZUFBZSxFQUFFLENBQUM7O0FBRXBCLFdBQU8sQ0FBQyxLQUFLLEVBQUUsQ0FBQyxJQUFJLENBQUMsVUFBQSxDQUFDO2FBQUksQ0FBQyxDQUFDLE9BQU8sQ0FBQyxLQUFLLEVBQUUsUUFBUSxDQUFDLElBQUksQ0FBQztLQUFBLENBQUMsQ0FBQztHQUMzRDs7O0FBZE8sZ0JBQVUscUJBQVYsVUFBVTs7QUFDVixxQkFBZSwwQkFBZixlQUFlOzs7QUFFdkIsZ0JBQVUsQ0FBQyxXQUFXLENBQUMsSUFBSSxlQUFlLEVBQUUsQ0FBQyxDQUFDO0FBQzlDLGdCQUFVLENBQUMsUUFBUSxDQUFDLFVBQVUsQ0FBQyxNQUFNLENBQUMsSUFBSSxDQUFDLENBQUMiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIvc3JjLyJ9
