f5c1ed10d4ef503b681b13c9eba54b0c82b5e517
Melissa Marshall <mveritym@gmail.com>
2016-06-18 15:28:59 +0100

    Adding rill/react middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import Rill from "rill";
 import compress from "@rill/compress";
 import serve from "@rill/static";
+import renderer from "@rill/react";
 
 import React from "react";
 import Renderer from "./server/renderer";
@@ -13,14 +14,19 @@ if (process.env.NODE_ENV === "development") {
   require("./server/hot-loader").listen(3001);
 }
 
-app.use(compress({
-  threshold: "2048kb"
-}));
-
+app.use(compress({ threshold: "2048kb" }));
 app.use(serve("public"));
+app.use(renderer());
+
+// Set locals in middleware.
+app.use(({ locals }, next) => {
+  locals.title = "@rill/react";
+  locals.options = { hotLoading };
+  return next();
+});
 
 app.use(({ req, res }, next) => {
-  res.body = Renderer.render({ hotLoading });
+  res.body = <Renderer />;
 });
 
 export default app;
--- server/renderer.js
+++ server/renderer.js
@@ -2,24 +2,22 @@ import React from "react";
 import { renderToString } from "react-dom/server";
 import App from "../components/app/app";
 
-export default class Renderer {
-  static render(options = {}) {
-    const scriptPath = options.hotLoading ? "http://localhost:3001/app.js" : "/app.js";
-    const stylesPath = options.hotLoading ? null : "/style.css";
+export default function Renderer(props, { locals }) {
+  const scriptPath = locals.hotLoading ? "http://localhost:3001/app.js" : "/app.js";
+  const stylesPath = locals.hotLoading ? null : "/style.css";
 
-    return renderToString(
-      <html>
-        <head>
-          <title>AstroView</title>
-          {stylesPath && <link rel="stylesheet" href={stylesPath} />}
-        </head>
-        <body>
-          <div className="app"><App/></div>
-          <script src="js/fits.js" />
-          <script src="js/fabric.js" />
-          <script src={scriptPath} />
-        </body>
-      </html>
-    );
-  }
+  return (
+    <html>
+      <head>
+        <title>AstroView</title>
+        {stylesPath && <link rel="stylesheet" href={stylesPath} />}
+      </head>
+      <body>
+        <div className="app"><App/></div>
+        <script src="js/fits.js" />
+        <script src="js/fabric.js" />
+        <script src={scriptPath} />
+      </body>
+    </html>
+  );
 }
